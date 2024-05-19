9e18f1cbfc35bc99f6ca195edc74609b5dfb5e5f
Michael Zhan <michaelzhan2001@gmail.com>
2024-05-18 23:16:40 -0500

    Add server side socket basic implementation

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,45 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 import dotenv from "dotenv";
+import http from 'http';
+import { Server, Socket } from 'socket.io';
 
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
+
+// set up socket
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  methods: ["GET", "POST"],
+}));
+
+const server: http.Server = http.createServer(app);
+server.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
+
+const io: Server = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST"],
+  }
+});
+
+io.on('connection', (socket: Socket) => {
+  console.log(`[socket]: ${socket.id} connected`);
+
+  io.to(socket.id).emit('message', `Hello ${socket.id}! You are the ${io.engine.clientsCount}th user`);
+
+  socket.on('disconnect', () => {
+    console.log(`[socket]: ${socket.id} disconnected`);
+  });
+})
+
+
+
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello world!");
-});
-
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
