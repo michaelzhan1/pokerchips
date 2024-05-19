df70c1c44d48eb728db8903395a01f44deedbdbf
Michael Zhan <michaelzhan2001@gmail.com>
2024-05-18 23:58:45 -0500

    Create small landing page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,32 +1,43 @@
 'use client';
 
 
-import { useState, useEffect } from 'react';
-import { socket } from '@/app/socket';
+import { useRouter } from 'next/navigation';
 
 
 export default function Home() {
-  useEffect(() => {
-    socket.connect();
-    socket.on('connect', () => {
-      console.log(`[client]: ${socket.id} connected`);
+  const router = useRouter();
 
-      socket.on('message', (message: string) => {
-        console.log(`[client]: ${message}`);
-      })
-    })
+  const createRoom = async () => {
+    const res = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + '/api/getNewRoomId');
+    const data = await res.text();
+    router.push(`${data}`);
+  }
 
-    return () => {
-      socket.disconnect();
-      socket.off('connect');
+  const joinRoom = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const roomId = (e.currentTarget as HTMLFormElement).roomId.value;
+    const res = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + `/api/checkRoomId/${roomId}`);
+    const data = await res.text();
+    if (data === 'true') {
+      router.push(`${roomId}`);
+    } else {
+      alert('Room does not exist');
     }
-  })
+  }
 
   return (
-    <main>
-      <p>
-        Hello, world!
-      </p>
-    </main>
+    <>
+      <div className='w-screen flex flex-col items-center'>
+        <div className='w-80'>
+          <button type='button' onClick={createRoom} className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Create Room</button>
+        </div>
+        <div className='w-80 mt-4'>
+          <form onSubmit={joinRoom}>
+            <input type='text' name='roomId' placeholder='Enter Room ID' className='w-full border border-gray-300 rounded p-2' />
+            <button type='submit' className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2'>Join Room</button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 }
