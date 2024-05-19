da2d8f4614151f9d4d8209e2657587d1b94cca85
Michael Zhan <michaelzhan2001@gmail.com>
2024-05-18 23:16:55 -0500

    Add client-side basic socket implementation

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,27 @@
+'use client';
+
+
+import { useState, useEffect } from 'react';
+import { socket } from '@/app/socket';
+
+
 export default function Home() {
+  useEffect(() => {
+    socket.connect();
+    socket.on('connect', () => {
+      console.log(`[client]: ${socket.id} connected`);
+
+      socket.on('message', (message: string) => {
+        console.log(`[client]: ${message}`);
+      })
+    })
+
+    return () => {
+      socket.disconnect();
+      socket.off('connect');
+    }
+  })
+
   return (
     <main>
       <p>
