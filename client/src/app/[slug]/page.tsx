dc6772ef3a8b8ca19593446f05bcc3601bd9b322
Michael Zhan <michaelzhan2001@gmail.com>
2024-05-19 00:56:43 -0500

    Allow user to join with custom chip amount

--- client/src/app/[slug]/page.tsx
+++ client/src/app/[slug]/page.tsx
@@ -7,15 +7,17 @@ import { socket } from '@/app/socket';
 
 export default function Page({params}: {params: {slug: string}}) {
   const roomId = params.slug;
-
+  
   useEffect(() => {
+    const amount = new URLSearchParams(window.location.search).get('amt');
+
     socket.connect();
     socket.on('connect', () => {
       console.log(`[client]: Connected with id ${socket.id}`);
       socket.on('message', (message: string) => {
         console.log(`[client]: ${message}`);
       })
-      socket.emit('joinRoom', roomId);
+      socket.emit('joinRoom', {roomId: roomId, amount: amount}); // todo: make button join with certain amount
 
     })
 
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -16,10 +16,11 @@ export default function Home() {
   const joinRoom = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const roomId = (e.currentTarget as HTMLFormElement).roomId.value;
+    const amt = (e.currentTarget as HTMLFormElement).amt.value;
     const res = await fetch(process.env.NEXT_PUBLIC_SERVER_URL + `/api/checkRoomId/${roomId}`);
     const data = await res.text();
     if (data === 'true') {
-      router.push(`${roomId}`);
+      router.push(`${roomId}?amt=${amt}`);
     } else {
       alert('Room does not exist');
     }
@@ -34,6 +35,7 @@ export default function Home() {
         <div className='w-80 mt-4'>
           <form onSubmit={joinRoom}>
             <input type='text' name='roomId' placeholder='Enter Room ID' className='w-full border border-gray-300 rounded p-2' />
+            <input type='number' name='amt' defaultValue='1000' className='w-full border border-gray-300 rounded p-2' />
             <button type='submit' className='w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2'>Join Room</button>
           </form>
         </div>
