4d2634bb371e28991541840f2702499c19540eb1
Michael Zhan <michaelzhan2001@gmail.com>
2024-05-19 13:53:29 -0500

    Add betting logic to client

--- client/src/app/[slug]/page.tsx
+++ client/src/app/[slug]/page.tsx
@@ -7,34 +7,52 @@ import { socket } from '@/app/socket';
 import { RoomInfoType } from '@/type/room';
 
 
+const DEFAULT_POT_AMOUNT = 0;
+
 export default function Page({params}: {params: {slug: string}}) {
   const router = useRouter();
   const roomId = params.slug;
 
-  const [players, setPlayers] = useState<string[]>([]);
-  const [chips, setChips] = useState<number[]>([]);
+  const [allPlayers, setAllPlayers] = useState<string[]>([]);
+  const [allChips, setAllChips] = useState<number[]>([]);
   const [pot, setPot] = useState<number>(0);
-  const [ownAmount, setOwnAmount] = useState<number>(0);
+  const [ownChips, setOwnChips] = useState<number>(0);
+  const [transactionAmt, setTransactionAmt] = useState<number>(0);
+
+  const handleTransactionAmtChange = (e: React.ChangeEvent<HTMLInputElement>): void => { setTransactionAmt(parseInt(e.target.value)); }
+
+  const handleBet = () => {
+    if (transactionAmt > ownChips) {
+      alert('Not enough chips');
+      return;
+    }
+    console.log(transactionAmt);
+    socket.emit('bet', {amt: transactionAmt, roomId: roomId, socketId: socket.id});
+  }
+
+  const handleTake = () => {
+    if (transactionAmt > pot) {
+      alert('Not enough chips in pot');
+      return;
+    }
+    socket.emit('take', {amt: transactionAmt, roomId: roomId, socketId: socket.id});
+  }
 
   const parseRoomInfo = (data: RoomInfoType, socketId: string | undefined) => {
     const playerData = Object.values(data);
     const players = playerData.map(player => player.name);
     const chips = playerData.map(player => player.amount);
-    const ownAmount = (socketId !== undefined) ? data[socketId].amount : 0;
-    setPlayers(players);
-    setChips(chips);
-    setOwnAmount(ownAmount);
+    const ownChips = (socketId !== undefined) ? data[socketId].amount : 0;
+    setAllPlayers(players);
+    setAllChips(chips);
+    setOwnChips(ownChips);
   }
 
-  const parsePotInfo = (data: number) => {
-    setPot(data);
-  }
-
+  const parsePotInfo = (data: number) => { setPot(data); }
   useEffect(() => {
-    console.log(socket.id);
     const searchParams = new URLSearchParams(window.location.search);
-    const amount = searchParams.get('amt') || '1000';
-    const name = searchParams.get('name') || 'First joiner';
+    const amount = searchParams.get('amt');
+    const name = searchParams.get('name');
 
     socket.connect();
     socket.on('connect', () => {
@@ -60,10 +78,18 @@ export default function Page({params}: {params: {slug: string}}) {
       <div className='w-screen flex flex-col items-center'>
         <div className='w-80'>
           <h1 className='text-2xl font-bold'>Room {roomId}</h1>
-          <p>Players: {players.join(', ')}</p>
-          <p>Chips: {chips.join(', ')}</p>
+          <p>Players: {allPlayers.join(', ')}</p>
+          <p>Chips: {allChips.join(', ')}</p>
           <p>Pot: {pot}</p>
-          <p>Your chips: {ownAmount}</p>
+          <p>Your chips: {ownChips}</p>
+        </div>
+        <hr className='h-px' />
+        <div className='flex flex-col'>
+          <input type='number' name='transactionAmt' onChange={handleTransactionAmtChange} defaultValue={DEFAULT_POT_AMOUNT} inputMode='numeric' className='w-80 border border-gray-300 rounded p-2' />
+        </div>
+        <div className='flex flex-col'>
+          <button type='button' onClick={handleBet} className='w-80 mb-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Bet</button>
+          <button type='button' onClick={handleTake} className='w-80 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Take</button>
         </div>
       </div>
     </main>
